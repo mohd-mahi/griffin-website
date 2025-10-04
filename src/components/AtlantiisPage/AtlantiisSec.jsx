1ae1057b9f158334c10c176dff26937b53a08d3a
Mohd Husain <mhusain@gmail.com>
2025-10-03 19:22:09 -1000

    created pages

--- src/components/AtlantiisPage/AtlantiisSec.jsx
+++ src/components/AtlantiisPage/AtlantiisSec.jsx
@@ -19,24 +19,19 @@ const AtlantiisSec = () => {
           />
           <div className="atlantic-text-wrapper">
             <div className="container">
-              <div className="row">
-                <div className="col-lg-6"></div>
-                <div className="col-lg-6">
-                  <div className="alt-content">
-                    <div className="alt-wrapper">
-                      <div className="alt-logo">
-                        <img
-                          src="/images/project-page/atlantiis-logo.png"
-                          alt="atlantiis logo"
-                        />
-                      </div>
-                      <div className="alt-des">
-                        <h2>A landmark</h2>
-                        <h3>address for visionaries</h3>
-                      </div>
-                      <h4>Grade ‘A’ office spaces & showrooms</h4>
-                    </div>
+              <div className="alt-content">
+                <div className="alt-wrapper">
+                  <div className="alt-logo">
+                    <img
+                      src="/images/project-page/atlantiis-logo.png"
+                      alt="atlantiis logo"
+                    />
                   </div>
+                  <div className="alt-des">
+                    <h2>A landmark</h2>
+                    <h3>address for visionaries</h3>
+                  </div>
+                  <h4>Grade ‘A’ office spaces & showrooms</h4>
                 </div>
               </div>
             </div>
